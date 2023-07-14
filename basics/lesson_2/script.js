4901a571d2bd4e90bb1ff02b52029ae351947658
igoriln <igorilnick@gmail.com>
2023-07-14 15:21:19 +0300

    Data types

---
+++ basics/lesson_2/script.js
@@ -0,0 +1,42 @@
+let message = "123";
+message = 321;
+
+alert(message);
+
+//number 
+
+let num = 123;
+num = 12.3;
+
+alert(num);
+
+//Infinity when devide to 0;
+alert(10/0);
+
+// NaN represents an error
+
+alert("Not a number"/ 10 * 2);
+
+//String value
+
+let str1 = "Double quotes";
+let str2 = 'Single quotes';
+let phrase = `Backtick are below at ESC key, can have another sthing in it: ${str1}`;
+
+alert(phrase);
+
+//Boolean type 
+
+const isTrue = true;
+
+alert(isTrue);
+
+//Null value
+
+let nullValue = null;
+
+//undefined value
+
+let something ;
+
+alert(something);
