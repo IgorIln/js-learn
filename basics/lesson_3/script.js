3f20e0b387bae1f136c690c82c046288af4f572d
igoriln <igorilnick@gmail.com>
2023-07-15 18:02:09 +0300

    lesson 3

---
+++ basics/lesson_3/script.js
@@ -0,0 +1,28 @@
+// Alert 
+
+alert("Hi there! =) This is alert");
+
+// Prompt 
+
+prompt("This is - Prompt - function");
+
+let result = prompt("Do you use Linux?","No");
+
+alert("Prompt accepts 2 values, watch code.")
+
+let distro = prompt("What linux distro do you use?", "Debian");
+
+alert(`You use ${distro}`);
+
+/*
+    Prompt function:
+
+    prompt(value, default value );
+
+*/
+
+// Confirm
+
+let study = confirm("Are you a student?");
+
+alert(`Confim: ${study}`);
