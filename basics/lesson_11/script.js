156001dbdb659ca34c4f532bab0568a825941852
igoriln <igorilnick@gmail.com>
2023-07-20 10:31:49 +0300

    lesson 11

---
+++ basics/lesson_11/script.js
@@ -0,0 +1,16 @@
+let result = Number(prompt(`How old are you?`, ``));
+
+
+switch (result) {
+    case 13:
+        alert(`Lucky 13`);
+        break;
+
+    case 15:
+        alert(`15 is good`);
+        break;
+
+    default:
+        alert(`Baby`);
+        break;
+}
